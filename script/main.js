1a5ed704d1b1c703e6bd889ccccacf07a388b00a
Elineu <elineutomas4@gmail.com>
2026-04-20 00:48:54 +0100

    create loadData and update renderVideoPrev func

--- script/main.js
+++ script/main.js
@@ -1,30 +1,45 @@
 let videoGridElement = document.getElementsByClassName("video-grid")[0];
 
-function renderVideoPrev() {
+function loadData() {
+  fetch('../data/videoInfo.json')
+  .then(response => {
+    if (!response.ok){
+      throw new Error('Erro ao carregar o ficheiro JSON')
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data)
+    renderVideoPrev(data);
+  })
+  .catch(error => console.log(error('Erro: ', error)));
+}
 
-  for (let i = 1; i <= 12; i++){
+function renderVideoPrev(data) {
+
+  for (let i = 0; i < data['video-prev'].length; i++){
   videoGridElement.innerHTML +=
   `<div class="video-prev">
         <div class="thumbnail-row">
-          <img src="thumbnails/thumbnail-${i}.webp" class="thumbnail" />
+          <img src=${data['video-prev'][i].img.thumbnail} class="thumbnail"/>
         </div>
         <div class="video-info-grid">
           <div class="channel-picture">
             <img
-              src="channel-pictures/channel-${i}.jpeg"
+              src=${data['video-prev'][i].img.profile}
               class="profile-picture"
             />
           </div>
           <div class="video-info">
             <p class="video-title">
-              Talking Tech and AI with Google CEO Sundar Pichai!
+              ${data['video-prev'][i].title}
             </p>
-            <p class="video-author">Marques Brownlee</p>
-            <p class="video-stats">3.4M views &#183; 6 months ago</p>
+            <p class="video-author">${data['video-prev'][i].author}</p>
+            <p class="video-stats">${data['video-prev'][i].stats}</p>
           </div>
         </div>
 </div>`;
   }
 }
 
-renderVideoPrev();
+loadData();
